62240622cdc346f9fe724701df19ce6e6d3077e9
Janki Moradiya <janki.moradiya@gmail.com>
2021-12-17 15:28:12 +0530

    added theme

--- react-src/public/index.php
+++ react-src/public/index.php
@@ -4,15 +4,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php $BRC_TEMPLATE_PATH = parse_url(get_template_directory_uri(), PHP_URL_PATH); ?>
-<script src='<?php echo $BRC_TEMPLATE_PATH; ?>/react-src/node_modules/@devloco/create-react-wptheme-utils/wpThemeClient.js'></script>
-
-<script src='<?php echo $BRC_TEMPLATE_PATH; ?>/react-src/node_modules/@devloco/create-react-wptheme-utils/wpThemeErrorOverlay.js'></script>
-
-<script> wpThemeClient.start("ws", "127.0.0.1", "8090"); </script>
-
 <meta charset="utf-8" />
-    <link rel="shortcut icon" href="/wp-content/themes/canyonlands/favicon.ico" />
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta
@@ -29,17 +22,17 @@
         If you're reading this from "view source" in your browser, it might not make sense as
         these tokens have already been evaluated and replaced, even in this remark blurb.
 
-        Notice the use of "php echo $TEMPLATE_PATH;" and /wp-content/themes/canyonlands in the tags above.
-        Both will be replaced with the URL of the `public` folder during the build (/wp-content/themes/canyonlands) or
+        Notice the use of "php echo $TEMPLATE_PATH;" and %PUBLIC_URL% in the tags above.
+        Both will be replaced with the URL of the `public` folder during the build (%PUBLIC_URL%) or
         at render time (php echo $TEMPLATE_PATH;)
         Only files inside the `public` folder can be referenced like this.
 
-        Unlike "/favicon.ico" or "favicon.ico", "/wp-content/themes/canyonlands/favicon.ico" will
+        Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
         work correctly both with client-side routing and a non-root public URL.
         Learn how to configure a non-root public URL by running `npm run wpbuild`.
     -->
     <title>React WordPress Theme</title>
-<link href="/wp-content/themes/canyonlands/static/css/main.chunk.css?00b8b5670fd942ebe8f3" rel="stylesheet"></head>
+</head>
     <body>
     <noscript>
         You need to enable JavaScript to run this app.
@@ -55,5 +48,5 @@
         To begin the development, run `npm run wpstart` or `yarn wpstart`.
         To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
     -->
-    <script src="/wp-content/themes/canyonlands/static/js/bundle.js?00b8b5670fd942ebe8f3"></script><script src="/wp-content/themes/canyonlands/static/js/0.chunk.js?00b8b5670fd942ebe8f3"></script><script src="/wp-content/themes/canyonlands/static/js/main.chunk.js?00b8b5670fd942ebe8f3"></script></body>
+    </body>
 </html>
